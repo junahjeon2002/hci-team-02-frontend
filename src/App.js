0b5e9862d46209d4d0c583e0f734ba7987a361bf
송원영 <rachelsong10@snu.ac.kr>
2025-05-24 03:37:43 +0900

    feat: add original page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NewsFeed from './NewsFeed';
 import ArticleDetail from './ArticleDetail';
 import Analog from './components/Analog';
+import Original from './components/Original';
 import TalkView from './TalkView';
 import OriginalArticleView from './OriginalArticleView';
 import { Routes, Route } from 'react-router-dom';
@@ -12,10 +13,10 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<NewsFeed />} />
-        <Route path="/article/:id" element={<Analog />} />
+        {<Route path="/article/:id" element={<Analog />} />}
         <Route path="/article/:id/analogy" element={<Analog />} />
         <Route path="/article/:id/talk" element={<TalkView />} />
-        <Route path="/article/:id/original" element={<OriginalArticleView />} />
+        <Route path="/article/:id/original" element={<Original />} />
       </Routes>
     </div>
   );
--- src/components/Analog.jsx
+++ src/components/Analog.jsx
@@ -31,16 +31,18 @@ export default function Analog() {
           <img src="/talkbox.svg" alt="톡" className="analog-btnicon" />
           톡 보기
         </button>
-        <button className="analog-btn">
-          원문보기
-          <img src="/script.svg" alt="원문보기" className="analog-btnicon" />
-        </button>
+        <Link to="/article/:id/original" style={{ textDecoration: 'none' }}>
+          <button className="analog-btn">
+            원문보기
+            <img src="/script.svg" alt="원문보기" className="analog-btnicon" />
+          </button>
+        </Link>
       </div>
       {/* Article */}
       <div className="analog-article">
         옛날 어느 마을에 진실을 전하는 <span className="analog-highlight-blue">'푸른 나팔'</span>이 있었어요.<br/>
         이 나팔은 조용하지만 언제나 바른 소리를 냈고, 마을 사람들은 중요한 소식이 궁금할 땐 이 나팔을 찾았죠.<br/>
-        시간이 흐르며 사람들은 <span className="analog-highlight-blue">화려한 복식</span>과 <span className="analog-highlight-blue">시끄러운 꽹과리</span>에 더 끌리게 되었지만, 중요한 순간마다 <span className="analog-highlight-pink">이야기꾼</span>들은 여전히 '푸른 나팔'을 꺼내들었어요.<br/>
+        시간이 흐르며 사람들은 <span className="analog-highlight-blue">화려한 복식</span>과 <span className="analog-highlight-blue">시끄러운 꽹과리</span>에 더 끌리게 되었지만, 중요한 순간마다 <span className="analog-highlight-blue">이야기꾼</span>들은 여전히 '푸른 나팔'을 꺼내들었어요.<br/>
         오래된 이야기는 새로운 이야기기도, 진짜 많이 필요할 때는 꼭 그 나팔이 등장했거든요.<br/>
         요즘 <span className="analog-highlight-blue">마을 연극</span>에서도 이 나팔은 조연처럼 자주 모습을 드러내며, 과거를 기억하게 하고 진실을 느끼게 해요.<br/>
         이제는 나팔을 드는 이들이 적어졌지만, 무대 위에서 울...
